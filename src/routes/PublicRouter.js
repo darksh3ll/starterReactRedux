827c4b4cc01fed28beebff6e8d22767b193905df
darksh3ll <energie.sud83@gmail.com>
2019-05-19 11:59:43 +0200

    rename dossier

--- src/routes/PublicRouter.js
+++ src/routes/PublicRouter.js
@@ -6,6 +6,8 @@ import NotFound from "../screens/NotFound";
 
 class PublicRouter extends Component {
     render() {
+
+
         return (
               <Switch>
                 <Route exact path="/" component={Home}/>
